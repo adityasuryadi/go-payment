ea36246c78dc5b3c28de15a152009cd59e6ab315
Aditya Suryadi <adityasuryaditya@gmail.com>
2023-08-24 10:06:44 +0700

    refactor config load env

--- config/config.go
+++ config/config.go
@@ -2,8 +2,10 @@ package config
 
 import (
 	"os"
+	"regexp"
 
 	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
 )
 
 type Config interface {
@@ -18,6 +20,19 @@ func (config *configImpl) Get(key string) string {
 }
 
 func New(filenames ...string) Config {
-	godotenv.Load(filenames...)
+	const projectDirName = "payment"
+	re := regexp.MustCompile(`^(.*` + projectDirName + `)`)
+	cwd, _ := os.Getwd()
+	rootPath := re.Find([]byte(cwd))
+
+	err := godotenv.Load(string(rootPath) + filenames[0])
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"cause": err,
+			"cwd":   cwd,
+		}).Fatal("Problem loading .env file")
+
+		os.Exit(-1)
+	}
 	return &configImpl{}
 }
